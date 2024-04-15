79ff0e17258a710d636404a464841dbbb6b061a7
mattprescotte <80309518+mattprescotte@users.noreply.github.com>
2024-04-15 08:49:12 -0400

    Update Q2.js

--- src/pages/Q2.js
+++ src/pages/Q2.js
@@ -15,54 +15,64 @@ import {render} from "@testing-library/react";
 // Sample chart data
 const pdata = [];
 const data = [];
-for (let num = 30*52; num >= 0; num -= 30){
-    pdata.push({
-        name: subDays(new Date(), num).toISOString().substr(0, 7),
-        MinorHomeVictimization: 50,
-        AllHomeVictimization: 10
-    })
-}
+
+try {
+    const response = await fetch('http://localhost:5000/home-victims?timeUnits=Month&standardColumn=True'); 
+    const jsonData = await response.json();
+    for (let num = 0; num < jsonData.length; num++){
+        pdata.push({
+            date_: jsonData[num][0],
+            MinorHomeVictimization: jsonData[num][1],
+            AllHomeVictimization: jsonData[num][2]
+        })
+    }
+
+  } catch (error) {
+    console.error('Unable to fetch data:', error);
+  }
 for (let num = 0; num < pdata.length; num++){
-    data.push(pdata[num])
+    data.push(pdata[num]) 
 }
+
+
 function Q2() {
-    const [user, setuser] = useState("")
-    const [pass, setpass] = useState("")
+    const [minDate, setMinDate] = useState("")
+    const [maxDate, setMaxDate] = useState("")
     const [status, setstatus] = useState("")
     const check_sub = () => {
-        if (user.length != 7 || pass.length != 7){
+        if (minDate.length != 7 || maxDate.length != 7){
             setstatus("Incorrect Input(s)")
         }
-        else if (user[4] != '-' || pass[4] != '-'){
+        else if (minDate[4] != '-' || maxDate[4] != '-'){
             setstatus("Incorrect Input(s)")
         }
-        else if ((user[5] != '0' && user[5] != '1') || (pass[5] != '0'  && pass[5] != '1')){
+        else if ((minDate[5] != '0' && minDate[5] != '1') || (maxDate[5] != '0'  && maxDate[5] != '1')){
             setstatus("Incorrect Input(s)")
         }
-        else if (user[5] == '0' && user[6] != '1' && user[6] != '2'
-            && user[6] != '3' && user[6] != '4' && user[6] != '5' && user[6] != '6'
-            && user[6] != '7' && user[6] != '8' && user[6] != '9'){
+        else if (minDate[5] == '0' && minDate[6] != '1' && minDate[6] != '2'
+            && minDate[6] != '3' && minDate[6] != '4' && minDate[6] != '5' && minDate[6] != '6'
+            && minDate[6] != '7' && minDate[6] != '8' && minDate[6] != '9'){
             setstatus("Incorrect Input(s)")
         }
-        else if (pass[5] == '0' && pass[6] != '1' && pass[6] != '2'
-            && pass[6] != '3' && pass[6] != '4' && pass[6] != '5' && pass[6] != '6'
-            && pass[6] != '7' && pass[6] != '8' && pass[6] != '9'){
+        else if (maxDate[5] == '0' && maxDate[6] != '1' && maxDate[6] != '2'
+            && maxDate[6] != '3' && maxDate[6] != '4' && maxDate[6] != '5' && maxDate[6] != '6'
+            && maxDate[6] != '7' && maxDate[6] != '8' && maxDate[6] != '9'){
             setstatus("Incorrect Input(s)")
         }
-        else if (pass[5] == '1' && pass[6] != '1' && pass[6] != '2' && pass[6] != '0'){
+        else if (maxDate[5] == '1' && maxDate[6] != '1' && maxDate[6] != '2' && maxDate[6] != '0'){
             setstatus("Incorrect Input(s)")
         }
-        else if (user[5] == '1' && user[6] != '1' && user[6] != '2' && user[6] != '0'){
+        else if (minDate[5] == '1' && minDate[6] != '1' && minDate[6] != '2' && minDate[6] != '0'){
             setstatus("Incorrect Input(s)")
         }
-        else if (user >= "2020-01" && user < "2024-04" && pass > "2020-01" && pass <= "2024-04"
-            && pass > user){
+        else if (minDate >= "2020-01" && minDate < "2024-04" && maxDate > "2020-01" && maxDate <= "2024-04"
+            && maxDate > minDate){
             setstatus("Correct Inputs")
             while(data.length > 0) {
                 data.pop();
             }
             for (let i = 0; i<pdata.length; i++){
-                if (pdata[i].name >= user && pdata[i].name <= pass){
+                if (pdata[i].date_ >= minDate && pdata[i].date_ <= maxDate){
                     data.push(pdata[i])
                 }
             }
@@ -76,11 +86,11 @@ function Q2() {
     }
     return (
         <>
-            <h1 className="text-heading">Query 2 Graph</h1>
+            <h1 className="text-heading">For minors and all victims, how has the percent of incidents happening in a residence changed since COVID? </h1>
             <ResponsiveContainer width="100%" aspect={3}>
                 <LineChart data={data} margin={{left: 20}}>
                     <CartesianGrid/>
-                    <XAxis dataKey="name" interval={"preserveStartEnd"}
+                    <XAxis dataKey="date_" interval={"preserveStartEnd"}
                            label={{value: 'Time (Month)', angle: 0, position: 'bottom'}}
                     />
                     <YAxis label={{value: 'Rate of Incidents', angle: -90, position: 'insideLeft'}}/>
@@ -103,12 +113,12 @@ function Q2() {
             <div className="Start">
                 <label>Start Month: </label>
                 <input type="text" placeholder="YYYY-MM"
-                       onChange={(e) => setuser(e.target.value)}/>
+                       onChange={(e) => setMinDate(e.target.value)}/>
             </div>
             <div className="Pass">
                 <label>End Month: </label>
                 <input type="text" placeholder="YYYY-MM"
-                       onChange={(e) => setpass(e.target.value)}/>
+                       onChange={(e) => setMaxDate(e.target.value)}/>
             </div>
             <div className="Sub">
                 <button onClick={check_sub}>Submit</button>
